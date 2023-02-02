5e86e8115d17d38022f06f6a2388d2cc183bf438
Ritsu <alsuldina@gmail.com>
2023-02-02 20:43:51 +0300

    добавлен цикл в sidebar (для растений)

--- about/index.php
+++ about/index.php
@@ -1,7 +1,9 @@
 <?include_once($_SERVER["DOCUMENT_ROOT"]."/app/header.php");?>
 
 <?
-echo "О проекте"
-?>
+$page = $REST_API->read($t_pages, "about");
 
+?>
+<h1 class="title"><?=$page["title"]?></h1>
+<?=$page["text"]?>
 <?include_once($_SERVER["DOCUMENT_ROOT"]."/app/footer.php");?>
